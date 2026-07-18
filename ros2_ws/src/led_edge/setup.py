4ee003da1baa34205a8309cad6c084c33f169039
Guillermo Hernández Facundo <023000292-sys>
2026-07-17 23:41:24 -0600

    Implementa listener ROS 2 para led_cmd

--- ros2_ws/src/led_edge/setup.py
+++ ros2_ws/src/led_edge/setup.py
@@ -7,25 +7,39 @@ setup(
     version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='UPSRJ Robotica',
     maintainer_email='jesus.loport@outlook.com',
-    description='Nodo edge de ejemplo: publica en /led_cmd para el ESP32 micro-ROS.',
+    description=(
+        'Nodo edge de ejemplo: publica y escucha mensajes '
+        'para el ESP32 micro-ROS.'
+    ),
     license='Apache-2.0',
     entry_points={
         'console_scripts': [
             'led_edge = led_edge.led_edge:main',
+
             # Uso: ros2 run led_edge blink
             'blink = led_edge.led_blink_publisher:main',
+
             # Uso: ros2 run led_edge control
             'control = led_edge.led_ctrl_publisher:main',
+
             # Uso: ros2 run led_edge gateway
             'gateway = led_edge.led_gateway:main',
+
+            # Uso: ros2 run led_edge listener
+            'listener = led_edge.led_listener:main',
         ],
     },
 )
